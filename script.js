da4392802458673be213ff0e5b9a3db542f85c6e
261129 <juana98510@gmail.com>
2026-01-03 01:02:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -299,3 +299,13 @@ function initMenu() {
     if (e.key === 'Escape') closeMenu();
   });
 }
+
+function setVh() {
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight * 0.01}px`
+  );
+}
+setVh();
+window.addEventListener('resize', setVh);
+window.addEventListener('orientationchange', setVh);
